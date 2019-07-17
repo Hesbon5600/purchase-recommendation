29296fcab1415de930873710ae50fbc87285db83
Hesbon5600 <hesbonkiptoo5600@gmail.com>
2019-07-17 09:14:06 +0300

    collaborative filtering models for recommending products to customers using purchase data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='collaborative filtering models for recommending products to customers using purchase data. In particular, we’ll cover in details the step-by-step process in constructing a recommendation system with Python and machine learning module Turicreate.',
+    author='Hesbon',
+    license='MIT',
+)
